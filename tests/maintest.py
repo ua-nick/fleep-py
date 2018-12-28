8569a9161183c1c7bc983ce143b02acdeb35ef01
Ivan Smirnov <Van10000@mail.ru>
2018-12-29 03:35:52 +0500

    Add old mp3 support

--- tests/maintest.py
+++ tests/maintest.py
@@ -1,12 +1,16 @@
 import fleep
 
-with open("testfile", "rb") as file:
-    info = fleep.get(file.read(128))
+def test_file(filename, type, extension, mime):
+    with open(filename, "rb") as file:
+        info = fleep.get(file.read(128))
 
-assert info.type == ["raster-image"]
-assert info.extension == ["png"]
-assert info.mime == ["image/png"]
+    assert info.type == [type]
+    assert info.extension == [extension]
+    assert info.mime == [mime]
 
-assert info.type_matches("raster-image")
-assert info.extension_matches("png")
-assert info.mime_matches("image/png")
+    assert info.type_matches(type)
+    assert info.extension_matches(extension)
+    assert info.mime_matches(mime)
+
+test_file("testfile", "raster-image", "png", "image/png")
+test_file("test.mp3", "audio", "mp3", "audio/mpeg")
